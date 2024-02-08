6db7c5b825508088751b69203ac179226db37b27
Matheus Dornelles <matheus.dorne@gmail.com>
2024-02-07 22:27:44 -0300

    Update Principal.java

--- src/main/java/br/com/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/TabelaFipe/principal/Principal.java
@@ -15,6 +15,21 @@ public class Principal {
                 "3 - CAMINHÃO \n" +
                 "Escolha uma opção: ");
         var escolha = leitura.nextInt();
+        switch (escolha) {
+            case 1 -> {
+                System.out.println("MOTO");
+                break;
+            }
+            case 2 -> {
+                System.out.println("CARRO");
+                break;
+            }
+            case 3 -> {
+                System.out.println("CAMINHÃO");
+                break;
+            }
+            default -> System.out.println("Opção inválida");
+        }
 
     }
 }
